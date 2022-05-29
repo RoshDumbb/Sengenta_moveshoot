ff0c655fbe9d972d081fe80494ef8730bfec8c80
RoshDumbb <roshanidumbhare30@gmail.com>
2022-05-29 19:52:26 +0530

    Changes in code

--- moveshoot/src/Components/Shoot.jsx
+++ moveshoot/src/Components/Shoot.jsx
@@ -1,114 +1,90 @@
-/** @format */
-
-import React, { useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Col, Container, Row } from "react-bootstrap";
-import "./Shoot.css";
+import React from 'react'
+import { useState } from 'react';
+import './Shoot.css'
 const Shoot = () => {
-  const [text, setText] = useState("");
-  const [getme, setget] = useState([])
-  
-  const handleChange = (e) => {
-    setText(e.target.value)
-  }
-  const handleSearch = (move) => {
-    if (text == 1 || text == 2 || text == 3 || text == 4 || text == 5) {
-      getme.filter((item)=> item !== move)
-    }
- }
+ const arr = [
+   { id: 1, color: Math.floor(Math.random() * 16777215).toString(16) },
+   { id: 2, color: Math.floor(Math.random() * 16777215).toString(16) },
+   { id: 3, color: Math.floor(Math.random() * 16777215).toString(16) },
+   { id: 4, color: Math.floor(Math.random() * 16777215).toString(16) },
+   { id: 5, color: Math.floor(Math.random() * 16777215).toString(16) },
+ ];
 
-  return (
-    <div onChange={handleChange}>
-      <Container>
-        <Row>
-          {/* for a fisrt div */}
+ const [box, setBox] = useState(arr);
+ const [inputValue, setInputValue] = useState(null);
+ const [emptyDiv, setEmptyDiv] = useState([]);
 
-          <Col>
-            <h6 style={{ marginTop: "150px", marginRight: "420px" }}>
-              empty div
-            </h6>
-            <div
-              style={{
-                border: "2px solid black",
-                width: "300px",
-                height: "320px",
-                // marginTop: "160px",
-              }}
-            ></div>
-          </Col>
+ // get input value from input box
+ const getValue = (v) => {
+   setInputValue(v);
+ };
 
-          <Col>
-            <h6 style={{ marginRight: "700px" }}>5 circles</h6>
-            <div onClick={handleSearch}  style={{ paddingRight: "700px" }}>
-              <Col>
-                <Row>
-                  {/* first button */}
-                  <Button
-                    className="shoot"
-                    style={{
-                      backgroundColor: "rgb(187, 239, 185)",
-                      borderColor: "green",
-                    }}
-                  >
-                    1
-                  </Button>
-                </Row>
-                {/* second button */}
-                <Row>
-                  <Button
-                    className="shoot"
-                    style={{
-                      backgroundColor: "rgb(239, 214, 192)",
-                      borderColor: "rgb(232, 150, 79)",
-                    }}
-                  >
-                    2
-                  </Button>
-                </Row>
-                {/* third button */}
-                <Row>
-                  <Button
-                    className="shoot"
-                    style={{
-                      backgroundColor: " rgb(73, 29, 176)",
-                      borderColor: " rgb(25, 15, 47)",
-                    }}
-                  >
-                    3
-                  </Button>
-                </Row>
-                {/*fourth button */}
-                <Row>
-                  <Button
-                    className="shoot"
-                    style={{
-                      backgroundColor: " rgb(171, 198, 237)",
-                      borderColor: " rgb(21, 50, 89)",
-                    }}
-                  >
-                    4
-                  </Button>
-                </Row>
-                {/* fifth button */}
-                <Row>
-                  <Button
-                    className="shoot"
-                    style={{
-                      backgroundColor: "rgb(223, 188, 237)",
-                      borderColor: " rgb(121, 8, 165)",
-                    }}
-                  >
-                    5
-                  </Button>
-                </Row>
-              </Col>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-      
-    </div>
-  );
-};
+ // when we click on button
+ const onSubmit = () => {
+   let val = box.find((item) => item.id === Number(inputValue));
+   if (val) {
+     let box2 = box.filter((item) => item.id !== Number(inputValue));
+     setBox([...box2]);
+     setEmptyDiv([...emptyDiv, val]);
+   }
+ };
 
-export default Shoot;
+ // send box from empty box to stack
+ const backToStack = (id) => {
+   let val2 = emptyDiv.find((item) => item.id === id);
+   if (val2) {
+     let box2 = emptyDiv.filter((item) => item.id !== id);
+     setEmptyDiv([...box2]);
+     setBox([...box, val2].sort((a, b) => a.id - b.id));
+   }
+ };
+
+ return (
+   <div className="App">
+     <div className="empty_div">
+       <p>Empty Div</p>
+       <div className="box">
+         {emptyDiv.length !== 0 &&
+           emptyDiv.map((item, index) => (
+             <div
+               className="ball"
+               key={item.id}
+               style={{ backgroundColor: `#${item.color}` }}
+               onClick={() => backToStack(item.id)}
+             >
+               {item.id}
+             </div>
+           ))}
+       </div>
+     </div>
+
+     {/* circles */}
+     <div className="circles">
+       {" "}
+       {box.map((item) => (
+         <div
+           className="ball"
+           key={item.id}
+           style={{ backgroundColor: `#${item.color}` }}
+         >
+           {item.id}
+         </div>
+       ))}
+     </div>
+
+     {/* input box */}
+     <div className="inputs">
+       <input
+         type="number"
+         name="number"
+         required
+         id=""
+         onChange={(e) => getValue(e.target.value)}
+       />
+       <button onClick={onSubmit}>Shoot</button>
+     </div>
+   </div>
+ );
+}
+
+export default Shoot
